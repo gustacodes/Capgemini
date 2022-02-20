cc2c55c58eba53ef65fa28c256e1b52310d8a1fc
Gustavo Henrique <86691048+gustacodes@users.noreply.github.com>
2022-02-20 02:06:56 -0300

    Update Senha.java

--- Senha.java
+++ Senha.java
@@ -19,7 +19,7 @@ public class Senha {
         String [] verificaNumeros = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         boolean numeros = false;
 
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < verificaNumeros.length; i++) {
             if(pass.contains(verificaNumeros[i])) {
                 numeros = true;
                 break;
